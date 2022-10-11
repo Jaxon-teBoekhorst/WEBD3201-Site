767d0f0b85006a6f6d5eeeadb61b527810a17015
Jaxon <jax.tebs@outlook.com>
2022-10-11 17:28:25 -0400

    idk

--- teboekhorstj/salespeople.php
+++ teboekhorstj/salespeople.php
@@ -16,11 +16,7 @@ require_once("./includes/header.php");
 
 $user = isset($_SESSION["current_user"]) ? isset($_SESSION["current_user"]) : "";
 
-if (isset($user)) {
+if ($user != "") {
     $user_type = $_SESSION['user_type'];
 }
 
-if (!str_contains($user_type, 'a')) {
-    setMessage("You do not have permission to enter this site");
-    redirect("./sign-in.php");
-}
